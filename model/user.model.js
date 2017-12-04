55e56b7bc940ab222693afe771abd94068e7931a
Twan van Maastricht <ajmaastr@avans.nl>
2017-12-04 13:01:57 +0100

    StarterPack nodejs

---
+++ model/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    title: String,
+    recipes: [{}]
+}, {
+    timestamps: true
+});
+
+
+const User = mongoose.model('user', UserSchema);
+
+// Add a 'dummy' user (every time you require this file!)
+const user = new User({
+    name: 'Joe',
+}).save();
+
+module.exports = User;
