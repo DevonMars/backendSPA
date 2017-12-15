399e91beb6ca6d50b1fafcea65e6837cf9206551
Twan van Maastricht <ajmaastr@avans.nl>
2017-12-15 10:37:36 +0100

    added short installation guide and my name to server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-//
-// server.js
-//
+/*
+
+Deze app is gemaakt door Twan van Maastricht 2113135
+ */
 var http = require('http');
 const mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-// var auth_routes_v1 = require('./api/authentication.routes.v1');
+
 var config = require('./config/env/env');
 
 var beer_routes_v1 = require('./routes/v1/beer.routes.v1');
 var city_routes_v1 = require('./routes/v1/city.routes.v1');
 var store_routes_v1 = require('./routes/v1/store.routes.v1');
 
-// var expressJWT = require('express-jwt');
-var app = express();
+
 
 
 if (process.env.NODE_ENV !== 'test') {
@@ -36,15 +36,6 @@ app.use(bodyParser.json({
     type: 'application/vnd.api+json'
 })); // parse application/vnd.api+json as json
 
-// Beveilig alle URL routes, tenzij het om /login of /register gaat.
-// app.use(expressJWT({
-//     secret: config.secretkey
-// }).unless({
-//     path: [
-//         { url: '/api/v1/login', methods: ['POST'] },
-//         { url: '/api/v1/register', methods: ['POST'] }
-//     ]
-// }));
 
 // configureer de app
 app.set('port', (process.env.PORT || config.env.webPort));
